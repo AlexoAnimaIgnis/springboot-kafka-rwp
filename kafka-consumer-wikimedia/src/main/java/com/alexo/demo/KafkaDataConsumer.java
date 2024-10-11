61a81409db22d28bb87909afe8d442e37241395d
AlexoAnimaIgnis <ballester.alexander@gmail.com>
2024-10-11 16:09:37 +0800

    implemented consumer

---
+++ kafka-consumer-wikimedia/src/main/java/com/alexo/demo/KafkaDataConsumer.java
@@ -0,0 +1,18 @@
+package com.alexo.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaDataConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaDataConsumer.class);
+
+    @KafkaListener(topics = "wikimedia_recentchange", groupId = "myGroup")
+    public void consume(String eventMessage) {
+        LOGGER.info("consume eventMessage: {}", eventMessage);
+    }
+
+}
