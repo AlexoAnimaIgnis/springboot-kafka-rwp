a674b9e1a667f9d53307f9b0a839f386fd66046c
AlexoAnimaIgnis <ballester.alexander@gmail.com>
2024-10-11 21:27:16 +0800

    implemented entity saver

--- kafka-consumer-wikimedia/src/main/java/com/alexo/demo/KafkaDataConsumer.java
+++ kafka-consumer-wikimedia/src/main/java/com/alexo/demo/KafkaDataConsumer.java
@@ -1,5 +1,7 @@
 package com.alexo.demo;
 
+import com.alexo.demo.entity.WikimediaData;
+import com.alexo.demo.repository.WikimediaDataRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -10,9 +12,18 @@ public class KafkaDataConsumer {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(KafkaDataConsumer.class);
 
+    private WikimediaDataRepository wikimediaDataRepository;
+
+    public KafkaDataConsumer(WikimediaDataRepository wikimediaDataRepository) {
+        this.wikimediaDataRepository = wikimediaDataRepository;
+    }
+
     @KafkaListener(topics = "wikimedia_recentchange", groupId = "myGroup")
     public void consume(String eventMessage) {
         LOGGER.info("consume eventMessage: {}", eventMessage);
+        WikimediaData wikimediaData = new WikimediaData();
+        wikimediaData.setWikiEventData(eventMessage);
+        wikimediaDataRepository.save(wikimediaData);
     }
 
 }
---
+++ kafka-consumer-wikimedia/src/main/java/com/alexo/demo/repository/WikimediaDataRepository.java
@@ -0,0 +1,9 @@
+package com.alexo.demo.repository;
+
+import com.alexo.demo.entity.WikimediaData;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WikimediaDataRepository extends JpaRepository<WikimediaData, Long> {
+}
